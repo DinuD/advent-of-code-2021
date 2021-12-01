117400a544710c97967716e85e200e7e55cfc0bb
Dinu-Ioan Dragomirescu <dinu-ioan.dragomirescu@arm.com>
2021-12-01 13:15:49 +0000

    Solve Day01 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        for(i in 1..input.lastIndex) {
+            if(input[i].toInt() > input[i-1].toInt())
+                increases++
+        }
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increases = 0
+        var sum = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        for(i in 1..input.lastIndex-2) {
+            if(input[i].toInt() + input[i+1].toInt() + input[i+2].toInt() > sum)
+                increases++
+            sum = input[i].toInt() + input[i+1].toInt() + input[i+2].toInt()
+        }
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
